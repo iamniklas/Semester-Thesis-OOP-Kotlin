49c04296ab530d98fea81e3224f99c1b0b5a7398
iamniklas <niklas.englmeier@web.de>
2022-01-10 19:20:39 +0100

    Updated Unit Tests // Test Coverage of around 80% for system relevant elements

--- src/test/kotlin/BankSystemTests.kt
+++ src/test/kotlin/BankSystemTests.kt
@@ -3,6 +3,8 @@ import accounttypes.StandardAccount
 import accounttypes.SuperPremiumAccount
 import accounttypes.TransactionType
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class BankSystemTests {
     @Test
@@ -18,7 +20,10 @@ class BankSystemTests {
             firstName = "Super Premium Account"
         }
 
-        bank.register(basicAccount)
+        assertEquals(0, bank.register(basicAccount), "Basic Account Registration failed")
+        assertEquals(0, bank.register(standardAccount), "Standard Account Registration failed")
+        assertEquals(0, bank.register(superPremiumAccount), "Super Premium Account Registration failed")
+
         println(bank.loggedInAs!!.firstName)
         bank.register(standardAccount)
         println(bank.loggedInAs!!.firstName)
@@ -38,6 +43,8 @@ class BankSystemTests {
         val info = bank.getAccountInfo()
 
         val expectedOutput = "Account Owner: Niklas Englmeier \nCard Identifier: ${bank.loggedInAs!!.accountIdentifier} \nAccount Balance: 100.0 \nPIN: 0000"
+
+        assertNotEquals(null, info, "Account Info empty!")
         if(info != expectedOutput) {
             error("Output invalid! \nExpected \n$expectedOutput\n, actual \n$info")
         }
@@ -57,12 +64,8 @@ class BankSystemTests {
 
         bank.logout()
 
-        if(bank.login(cardId, "1235") == 0 && !bank.requireLogin()) {
-            error("This should not have happened")
-        }
-        if(bank.login(cardId, "1234") != 0 && bank.requireLogin()) {
-            error("This should not have happened")
-        }
+        assertEquals(false, bank.login(cardId, "1235") == 0 && !bank.requireLogin(), "Login successful, but shouldn't be")
+        assertEquals(true, bank.login(cardId, "1234") == 0 && bank.requireLogin(), "Login not successful, but should be")
     }
 
     @Test
@@ -76,9 +79,7 @@ class BankSystemTests {
 
         bank.register(account)
 
-        if(bank.registeredAccounts.size != 1) {
-            error("Invalid Account Registry Size")
-        }
+        assertEquals(1, bank.registeredAccounts.size, "Invalid Account Registry Size")
     }
 
     @Test
@@ -92,16 +93,12 @@ class BankSystemTests {
 
         bank.register(account)
 
-        if(bank.deposit(50.0f, TransactionType.ATM) != 0 || bank.loggedInAs!!.accountBalance != 100.0f) {
-            error("Deposit fail")
-        }
-        if(bank.withdraw(50.0f, TransactionType.ATM) != 0 || bank.loggedInAs!!.accountBalance != 50.0f) {
-            error("Withdraw fail")
-        }
+        assertEquals(true, bank.deposit(50.0f, TransactionType.ATM) == 0 && bank.loggedInAs!!.accountBalance == 100.0f, "Deposit fail")
+        assertEquals(true, bank.withdraw(50.0f, TransactionType.ATM) == 0 || bank.loggedInAs!!.accountBalance == 50.0f, "Withdraw fail")
     }
 
     @Test
-    fun testTransferDepositAndWithdraw() {
+    fun testMoneyTransfer() {
         val bank: Bank = Bank()
         var cardId1 = ""
         val cardPin1 = "0000"
@@ -114,7 +111,7 @@ class BankSystemTests {
             pin = cardPin1
         }
 
-        bank.register(account1)
+        assertEquals(0, bank.register(account1), "Account 1 Registration Failed")
         cardId1 = bank.loggedInAs!!.accountIdentifier
         println(bank.getAccountInfo())
         println("---")
@@ -131,25 +128,21 @@ class BankSystemTests {
 
         Thread.sleep(2000)
 
-        bank.register(account2)
+        assertEquals(0, bank.register(account2), "Account 2 Registration Failed")
         cardId2 = bank.loggedInAs!!.accountIdentifier
         println(bank.getAccountInfo())
         println("---")
 
-        bank.transfer(cardId1, 50.0f)
+        assertEquals(0, bank.transfer(cardId1, 50.0f), "Transfer failed")
 
         if(bank.loggedInAs!!.accountBalance != 0.0f) {
             error("Transfer receive error. Expected 0.0, actual ${bank.loggedInAs!!.accountBalance}")
         }
 
-        //TODO Test deposit & withdraw here
-
         bank.logout()
         bank.login(cardId1, cardPin1)
         println("---")
         println("Account 1 Balance")
-        if(bank.loggedInAs!!.accountBalance != 100.0f) {
-            error("Transfer receive error. Expected 100.0, actual ${bank.loggedInAs!!.accountBalance}")
-        }
+        assertEquals(100.0f, bank.loggedInAs!!.accountBalance, "Transfer receive error. Expected 100.0, actual ${bank.loggedInAs!!.accountBalance}")
     }
 }
